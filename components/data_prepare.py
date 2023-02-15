22a2f286d5f797d69c7880c19555061d07658f18
malkova.ka <kristina.malkova@domru.ru>
2023-02-16 02:53:58 +0500

    data cleaning

--- components/data_prepare.py
+++ components/data_prepare.py
@@ -1,23 +1,72 @@
 import os
 import pandas as pd
 from datetime import datetime
+from fuzzywuzzy import process
 
 from components.constants import DATA_FILE, DATA_DIR, FILTER_SUBSTR, DATE_FORMAT_MONTH, DATE_FORMAT_ISO, \
-    BASE_DATA_COLUMNS, ALL_VALUES_FLAG
+    BASE_DATA_COLUMNS, ALL_VALUES_FLAG, MATCH_RATIO, MIN_WORD_LEN
 
 
 def get_data() -> pd.DataFrame():
     data_path = os.path.join(DATA_DIR, DATA_FILE)
     data = pd.read_csv(data_path)
+    data = clean_data(data)
+    return data
+
+
+def clean_data(data: pd.DataFrame):
     data = data.loc[data['PAID_AMOUNT'] > 0]
     data['MONTH_DT'] = pd.to_datetime(data['MONTH'], format=DATE_FORMAT_MONTH, errors='coerce').dt.date
     data = data.dropna()
     data['CLAIM_SPECIALTY'] = data['CLAIM_SPECIALTY'].str.upper().str.strip()
     for item in FILTER_SUBSTR:
         data['CLAIM_SPECIALTY'] = data['CLAIM_SPECIALTY'].str.replace(item, ' ', regex=True)
-    data['CLAIM_SPECIALTY'] = data['CLAIM_SPECIALTY'].apply(lambda x: ' '.join(str(x).split()))
-    data['SERVICE_CATEGORY'] = data['SERVICE_CATEGORY'].str.replace('SpecialistFFS', 'SpecialistsFFS', regex=True)
+    data['CLAIM_SPECIALTY_SPLIT'] = data['CLAIM_SPECIALTY'].apply(lambda x: str(x).split())
+    data['CLAIM_SPECIALTY'] = data['CLAIM_SPECIALTY_SPLIT'].apply(lambda x: ' '.join(sorted(set(x), key=x.index)))
+    del data['CLAIM_SPECIALTY_SPLIT']
+    data['SERVICE_CATEGORY'] = data['SERVICE_CATEGORY'].replace('SpecialistFFS', 'SpecialistsFFS')
+    data['CLAIM_SPECIALTY'] = data['CLAIM_SPECIALTY'].replace({'SURGICAL': 'SURGERY',
+                                                               'ORTHOPAEDIC': 'ORTHOPEDIC'}, regex=True)
+    match_map = match_rows(set(data['CLAIM_SPECIALTY'].tolist()))
+    data['CLAIM_SPECIALTY'] = data['CLAIM_SPECIALTY'].replace(match_map)
+    substr_map = find_substrings(set(data['CLAIM_SPECIALTY'].tolist()))
+    data['CLAIM_SPECIALTY'] = data['CLAIM_SPECIALTY'].replace(substr_map)
     return data
+
+
+def find_substrings(vals: set) -> dict:
+    vals = sorted(vals)
+    match_map = {}
+    for index, val in enumerate(vals[:-1]):
+        for next_index in range(index + 1, len(vals)):
+            if vals[next_index].find(val, 0, len(val)) < 0:
+                break
+            if vals[next_index][len(val)] == ' ':
+                continue
+            if match_map.get(val) and len(match_map.get(val)) <= len(vals[next_index]):
+                continue
+            match_map[val] = vals[next_index]
+    for item in list(set(match_map.keys()) & set(match_map.values())):
+        match_map.pop(item)
+    return match_map
+
+
+def match_rows(vals: set) -> dict:
+    results = {}
+    vals = sorted(list(filter(lambda x: len(x) >= MIN_WORD_LEN, vals)))
+    for val in vals:
+        res = process.extract(val, vals, limit=4)
+        results[val] = res[1:]
+    match_map = {}
+    for val in vals:
+        if val in match_map.keys():
+            continue
+        similar_combinations = results.get(val, [])
+        for similar_comb, ratio in similar_combinations:
+            if ratio <= MATCH_RATIO:
+                continue
+            match_map[similar_comb] = val
+    return match_map
 
 
 def get_column_vals(data: pd.DataFrame, column_name: str) -> list:
